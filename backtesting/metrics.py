cc0bea93d47a787807933fbf17801b012954ebc9
Aditya Chaudhary <aditya67539@gmail.com>
2025-11-18 22:53:43 +0530

    feat: Add Sharpe ratio metric in compute_metrics

--- backtesting/metrics.py
+++ backtesting/metrics.py
@@ -8,14 +8,14 @@ def compute_metrics(equity, initial_balance):
     cagr = (equity.iloc[-1] / initial_balance) ** (1 / years) - 1
     print(f"CAGR: {cagr * 100}%")
 
-    # # Sharpe ratio
-    # daily_returns = equity.pct_change().dropna()
+    # Sharpe ratio
+    daily_returns = equity.pct_change().dropna()
 
-    # mean = daily_returns.mean()
-    # std = daily_returns.std()
+    mean = daily_returns.mean()
+    std = daily_returns.std()
 
-    # sharpe_ratio = (mean / std) * np.sqrt(252)
-    # print(f"Sharpe Ratio: {sharpe_ratio}")
+    sharpe_ratio = (mean / std) * np.sqrt(252)
+    print(f"Sharpe Ratio: {sharpe_ratio}")
 
     # # Max Drawdown
     # rolling_max = equity.cummax()
